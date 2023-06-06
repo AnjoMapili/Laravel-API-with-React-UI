67104de96ed1d95035467362b5fdfe4138657d97
AnjoMapili <anjomapili80@yahoo.com>
2023-06-06 13:11:56 +0800

    Update SkillCreate.jsx

--- react-js-ui/src/components/skills/SkillCreate.jsx
+++ react-js-ui/src/components/skills/SkillCreate.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function SkillCreate() {
-  return <div>SkillCreate</div>;
+  return (
+    <div className="mt-12">
+      <form className="max-w-md mx-auto p-4 bg-white shadow-md rounded-sm">
+        <div className="space-y-6">
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2 text-sm font-medium">
+              Name
+            </label>
+            <input
+              className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2 "
+              type="text"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="lastname"
+              className="block mb-2 text-sm font-medium"
+            >
+              LastName
+            </label>
+            <input
+              className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2 "
+              type="text"
+            />
+          </div>
+        </div>
+        <div className="my-4">
+          <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">
+            Store
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
