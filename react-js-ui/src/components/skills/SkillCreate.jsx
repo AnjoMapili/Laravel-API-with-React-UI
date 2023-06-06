624980e75be754d4d479bd246aa536b4e903552d
AnjoMapili <anjomapili80@yahoo.com>
2023-06-06 12:08:44 +0800

    SkillsSetup

---
+++ react-js-ui/src/components/skills/SkillCreate.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SkillCreate() {
+  return <div>SkillCreate</div>;
+}
---
+++ react-js-ui/src/components/skills/SkillIndex.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SkillIndex() {
+  return <div>SkillIndex</div>;
+}
